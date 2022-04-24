62eb79d8bf45f19a6981758635882418b45dee08
nabetama <mao.nabeta@gmail.com>
2022-04-24 10:08:35 +0900

    update

--- go/install.sh
+++ go/install.sh
@@ -2,7 +2,7 @@
 
 GOVERSION=${1}
 if [ -z "${GOVERSION}" ]; then
-	GOVERSION=1.15.6
+	GOVERSION=1.17.7
 fi
 
 if [ -d ${HOME}/.go/${GOVERSION} ]; then
@@ -12,7 +12,7 @@ fi
 
 rm -rf ${GOHOME}/pkg
 mkdir -p ${HOME}/.go/${GOVERSION}
-curl https://storage.googleapis.com/golang/go${GOVERSION}.darwin-amd64.tar.gz \
+curl https://storage.googleapis.com/golang/go${GOVERSION}.darwin-arm64.tar.gz \
 	| tar xvzf - -C ${HOME}/.go/${GOVERSION}/ --strip-components=1
 echo $GOVERSION > ${HOME}/.go/.goversion
 
