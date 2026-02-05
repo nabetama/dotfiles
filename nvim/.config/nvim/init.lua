889cf4d71d5f865a26c54d2ed1396e3f5266f73f
nabetama <mao.nabeta@gmail.com>
2026-02-05 22:17:40 +0900

    ,e でエラー見る

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -96,6 +96,8 @@ require("lazy").setup({
           vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
           vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
           vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
         end,
       })
     end
